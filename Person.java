a9e52f7f931b194343eb06af2be700246542a62b
Tugu8 <tugutugu018@gmail.com>
2025-06-02 12:10:42 +0800

    Initial commit

--- Person.java
+++ Person.java
@@ -1,6 +1,7 @@
+
 class Person {
-    String name;
-    int age;
+    String name; 
+    int age;  
 
     void greet() {
         System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
