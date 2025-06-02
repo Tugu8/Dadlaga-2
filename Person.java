dcc231cae2ae285b66b54029464543545c6500c4
Tugu8 <tugutugu018@gmail.com>
2025-06-02 11:50:35 +0800

    dadlaga

---
+++ Person.java
@@ -0,0 +1,24 @@
+class Person {
+    String name;
+    int age;
+
+    void greet() {
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setName("Bat");
+        p.setAge(25);
+        p.greet();
+    }
+}
